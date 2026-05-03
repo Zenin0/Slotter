9149031b51271f00beb3e049716f3e34ce04a746
isaac <isaacsanz@proton.me>
2026-05-03 14:45:03 +0200

    feat(newURI): slotter new URI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+
+  server : {
+    allowedHosts: ["open.srvisaac.work", "slotter.srvisaac.work"]
+  },
+
   plugins: [
     react(),
     tailwindcss(),
